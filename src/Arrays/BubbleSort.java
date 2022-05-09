fc70e0f8649de533af3aa24bcd4607e5bed37bc9
Garima Gautam <80679679+GarimaGautam116@users.noreply.github.com>
2022-05-09 18:24:09 +0530

    java program

--- src/Arrays/BubbleSort.java
+++ src/Arrays/BubbleSort.java
@@ -1,14 +1,17 @@
 package Arrays;
 import java.util.Scanner;
 public class BubbleSort{
-    public static void bubbleSort(int arr[]){
-        for(int counter=0;counter<arr.length-1;counter++){
-            for(int j =0;j<arr.length-1-counter;j++){
-                if(arr[j]>arr[j+1]){
+    public static void bubbleSort(int arr[])
+    {
+        for(int counter=0;counter<arr.length-1;counter++)
+        {
+            for(int j =0;j<arr.length-1-counter;j++)
+            {
+                if(arr[j]>arr[j+1]) {
                     int temp = arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
-                }1
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
             }
         }
     }
