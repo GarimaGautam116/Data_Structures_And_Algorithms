13b91862862f2a7946979ef5283de810b23f8f02
Garima Gautam <80679679+GarimaGautam116@users.noreply.github.com>
2022-05-09 18:50:10 +0530

    java program

---
+++ src/Arrays/SelectionSort.java
@@ -0,0 +1,34 @@
+package Arrays;
+import java.util.Scanner;
+
+public class SelectionSort {
+    public static void selectionsort(int[] arr){
+        for(int counter =0;counter<arr.length-1;counter++){
+           int min = counter;
+            for(int j=counter+1;j<arr.length;j++){
+                if(arr[j] < arr[min]){
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min]=arr[counter];
+            arr[counter]=temp;
+        }
+    }
+    public static void display(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + "  ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size of array");
+        int arr[] = new int[sc.nextInt()];
+        System.out.println("enter the elements of array");
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        selectionsort(arr);
+        display(arr);
+    }
+}
